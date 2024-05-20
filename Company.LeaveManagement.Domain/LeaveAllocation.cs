d1e3522783344393feb609f97da9d7ed8c7463c5
durpintm <durpinthapa@gmail.com>
2024-05-19 22:14:45 -0400

    Company Leave Management Application Init

---
+++ Company.LeaveManagement.Domain/LeaveAllocation.cs
@@ -0,0 +1,18 @@
+﻿using Company.LeaveManagement.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.LeaveManagement.Domain
+{
+    internal class LeaveAllocation: BaseDomainEntity
+    {
+        public int NumberOfDays { get; set; }
+        public LeaveType LeaveType { get; set; }
+        public int LeaveTypeId { get; set; }
+        public int Period { get; set; }
+    }
+}
---
+++ Company.LeaveManagement.Domain/LeaveType.cs
@@ -0,0 +1,10 @@
+﻿using Company.LeaveManagement.Domain.Common;
+
+namespace Company.LeaveManagement.Domain
+{
+    public class LeaveType: BaseDomainEntity
+    {
+        public string Name { get; set; } 
+        public int DefaultDays { get; set; }
+    }
+}
