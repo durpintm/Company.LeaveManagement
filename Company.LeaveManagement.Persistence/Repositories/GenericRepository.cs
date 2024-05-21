76e4d20cb77ef2c7e1eb1f29131bf33c7d488538
durpintm <durpinthapa@gmail.com>
2024-05-21 19:12:39 -0400

    Implemented Generic Repository

---
+++ Company.LeaveManagement.Persistence/Repositories/GenericRepository.cs
@@ -0,0 +1,50 @@
+﻿using Company.LeaveManagement.Application.Persistence.Contracts;
+using Microsoft.EntityFrameworkCore;
+
+namespace Company.LeaveManagement.Persistence.Repositories
+{
+    internal class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly LeaveManagementDbContext _dbContext;
+
+        public GenericRepository(LeaveManagementDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task<T> Add(T entity)
+        {
+            await _dbContext.AddAsync(entity);
+            await _dbContext.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task Delete(T entity)
+        {
+            _dbContext.Set<T>().Remove(entity);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task<bool> Exists(int id)
+        {
+            var entity = await Get(id);
+            return entity != null;
+        }
+
+        public async Task<T> Get(int id)
+        {
+            return await _dbContext.Set<T>().FindAsync();
+        }
+
+        public async Task<IReadOnlyList<T>> GetAll()
+        {
+            return await _dbContext.Set<T>().ToListAsync();
+        }
+
+        public async Task Update(T entity)
+        {
+            _dbContext.Entry(entity).State = EntityState.Modified;
+
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
