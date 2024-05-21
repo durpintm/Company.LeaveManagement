e628dd48994b93ce3657b41399325a5daf7ea557
durpintm <durpinthapa@gmail.com>
2024-05-21 19:03:07 -0400

    Added ef core in the project

---
+++ Company.LeaveManagement.Persistence/LeaveManagementDbContext.cs
@@ -0,0 +1,38 @@
+﻿using Company.LeaveManagement.Domain;
+using Company.LeaveManagement.Domain.Common;
+using Microsoft.EntityFrameworkCore;
+
+namespace Company.LeaveManagement.Persistence
+{
+    public class LeaveManagementDbContext: DbContext
+    {
+        public LeaveManagementDbContext(DbContextOptions<LeaveManagementDbContext> options): base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(LeaveManagementDbContext).Assembly);
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            foreach (var entry in ChangeTracker.Entries<BaseDomainEntity>())
+            {
+                entry.Entity.LastModifiedDate = DateTime.Now;
+
+                if(entry.State == EntityState.Added)
+                {
+                    entry.Entity.DateCreated = DateTime.Now;
+                }
+            }
+
+            return base.SaveChangesAsync(cancellationToken);
+        }
+
+        public DbSet<LeaveType> LeaveTypes { get; set; }
+        public DbSet<LeaveRequest> LeaveRequests { get; set; }
+        public DbSet<LeaveAllocation> LeaveAllocations { get; set; }
+    }
+}
