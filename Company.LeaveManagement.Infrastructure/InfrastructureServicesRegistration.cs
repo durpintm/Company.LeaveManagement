c243d6d4659bbe98fda58dfc1b0bc2133241f363
durpintm <durpinthapa@gmail.com>
2024-05-22 01:14:55 -0400

    Register email sender in DI container

--- Company.LeaveManagement.Infrastructure/InfrastructureServicesRegistration.cs
+++ Company.LeaveManagement.Infrastructure/InfrastructureServicesRegistration.cs
@@ -1,12 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Company.LeaveManagement.Application.Contracts.Infrastructure;
+using Company.LeaveManagement.Application.Models;
+using Company.LeaveManagement.Infrastructure.Mail;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Company.LeaveManagement.Infrastructure
 {
-    public class InfrastructureServicesRegistration
+    public static class InfrastructureServicesRegistration
     {
+        public static IServiceCollection ConfigureInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<EmailSettings>(configuration.GetSection("EmailSettings"));
+            services.AddTransient<IEmailSender, EmailSender>();
+            return services;
+        }
     }
 }
