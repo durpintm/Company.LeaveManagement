6d56d3576b32038c165b334aaba89d4a684c0f4a
durpintm <durpinthapa@gmail.com>
2024-05-22 01:05:28 -0400

    Implementing email sender

---
+++ Company.LeaveManagement.Application/Contracts/Infrastructure/IEmailSender.cs
@@ -0,0 +1,9 @@
+﻿using Company.LeaveManagement.Application.Models;
+
+namespace Company.LeaveManagement.Application.Contracts.Infrastructure
+{
+    public interface IEmailSender
+    {
+        Task<bool> SendEmail(Email email);
+    }
+}
--- Company.LeaveManagement.Application/Features/LeaveRequests/Handlers/Commands/CreateLeaveRequestCommandHandler.cs
+++ Company.LeaveManagement.Application/Features/LeaveRequests/Handlers/Commands/CreateLeaveRequestCommandHandler.cs
@@ -5,6 +5,8 @@ using Company.LeaveManagement.Application.Contracts.Persistence;
 using Company.LeaveManagement.Application.Responses;
 using Company.LeaveManagement.Domain;
 using MediatR;
+using Company.LeaveManagement.Application.Contracts.Infrastructure;
+using Company.LeaveManagement.Application.Models;
 
 namespace Company.LeaveManagement.Application.Features.LeaveRequests.Handlers.Commands
 {
@@ -12,11 +14,16 @@ namespace Company.LeaveManagement.Application.Features.LeaveRequests.Handlers.Co
     {
         private readonly ILeaveRequestRepository _leaveRequestRepository;
         private readonly IMapper _mapper;
+        private readonly IEmailSender _emailSender;
 
-        public CreateLeaveRequestCommandHandler(ILeaveRequestRepository leaveRequestRepository, IMapper mapper)
+        public CreateLeaveRequestCommandHandler(
+            ILeaveRequestRepository leaveRequestRepository,
+            IMapper mapper,
+            IEmailSender emailSender)
         {
             _leaveRequestRepository = leaveRequestRepository;
             _mapper = mapper;
+            _emailSender = emailSender;
         }
 
         public async Task<BaseCommandResponse> Handle(CreateLeaveRequestCommand request, CancellationToken cancellationToken)
@@ -37,6 +44,22 @@ namespace Company.LeaveManagement.Application.Features.LeaveRequests.Handlers.Co
             response.Success = true;
             response.Message = "Creation Successful";
             response.Id = leaveRequest.Id;
+
+            var email = new Email()
+            {
+                To = "employee@org.com",
+                Body = $"Your leave request for {request.LeaveRequestDto.StartDate:D} to {request.LeaveRequestDto.EndDate:D} " +
+                $"has been submitted successfully.",
+                Subject = "Leave Request Submitted"
+            };
+
+            try
+            {
+                await _emailSender.SendEmail(email);
+            }catch(Exception ex)
+            {
+                // log or handle error, but do not throw
+            }
 
             return response;
         }
