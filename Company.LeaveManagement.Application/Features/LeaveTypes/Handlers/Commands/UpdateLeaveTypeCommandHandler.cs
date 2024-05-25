0730cab31cc396df51b6d57017f2d3ff154b455a
durpintm <durpinthapa@gmail.com>
2024-05-24 23:39:10 -0400

    Setting BaseCommandResponse in Update Leave Type Command

--- Company.LeaveManagement.Application/Features/LeaveTypes/Handlers/Commands/UpdateLeaveTypeCommandHandler.cs
+++ Company.LeaveManagement.Application/Features/LeaveTypes/Handlers/Commands/UpdateLeaveTypeCommandHandler.cs
@@ -4,10 +4,11 @@ using Company.LeaveManagement.Application.Exceptions;
 using Company.LeaveManagement.Application.Features.LeaveTypes.Requests.Commands;
 using Company.LeaveManagement.Application.Contracts.Persistence;
 using MediatR;
+using Company.LeaveManagement.Application.Responses;
 
 namespace Company.LeaveManagement.Application.Features.LeaveTypes.Handlers.Commands
 {
-    public class UpdateLeaveTypeCommandHandler : IRequestHandler<UpdateLeaveTypeCommand, Unit>
+    public class UpdateLeaveTypeCommandHandler : IRequestHandler<UpdateLeaveTypeCommand, BaseCommandResponse>
     {
         private readonly ILeaveTypeRepository _leaveTypeRepository;
         private readonly IMapper _mapper;
@@ -17,19 +18,28 @@ namespace Company.LeaveManagement.Application.Features.LeaveTypes.Handlers.Comma
             _leaveTypeRepository = leaveTypeRepository;
             _mapper = mapper;
         }
-        public async Task<Unit> Handle(UpdateLeaveTypeCommand request, CancellationToken cancellationToken)
+        public async Task<BaseCommandResponse> Handle(UpdateLeaveTypeCommand request, CancellationToken cancellationToken)
         {
+            var response = new BaseCommandResponse();
             var validator = new UpdateLeaveTypeDtoValidator();
             var validationResult = await validator.ValidateAsync(request.LeaveTypeDto);
 
             if (!validationResult.IsValid)
             {
-                throw new ValidationException(validationResult);
+                response.Success = false;
+                response.Message = "Creation Failed";
+                response.Errors = validationResult.Errors.Select(q => q.ErrorMessage).ToList();
+                return response;
             }
+
             var leaveType = await _leaveTypeRepository.Get(request.LeaveTypeDto.Id);
             _mapper.Map(request.LeaveTypeDto, leaveType);
             await _leaveTypeRepository.Update(leaveType);
-            return Unit.Value;
+
+            response.Success = true;
+            response.Message = "Creation Successful";
+            response.Id = leaveType.Id;
+            return response;
         }
     }
 }
--- Company.LeaveManagement.Application/Features/LeaveTypes/Requests/Commands/UpdateLeaveTypeCommand.cs
+++ Company.LeaveManagement.Application/Features/LeaveTypes/Requests/Commands/UpdateLeaveTypeCommand.cs
@@ -1,9 +1,10 @@
 ﻿using Company.LeaveManagement.Application.DTOs.LeaveType;
+using Company.LeaveManagement.Application.Responses;
 using MediatR;
 
 namespace Company.LeaveManagement.Application.Features.LeaveTypes.Requests.Commands
 {
-    public class UpdateLeaveTypeCommand: IRequest<Unit>
+    public class UpdateLeaveTypeCommand: IRequest<BaseCommandResponse>
     {
         public LeaveTypeDto LeaveTypeDto { get; set; }
     }
